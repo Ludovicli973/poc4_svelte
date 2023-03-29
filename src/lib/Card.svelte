ba8ac12863ad11839c8d5d711b601a86c2d7d2e9
IssamLynx <hm_bakha@esi.dz>
2023-03-29 16:08:59 +0200

    Finished home page

---
+++ src/lib/Card.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let anime;
+</script>
+
+<a href={`/details/${anime._id}`}>
+	<div class="w-[200px] h-[270px] my-1">
+		<img src={anime.image} alt="poster" class="w-full h-full object-cover" />
+		<p>{anime.title}</p>
+		<p>{anime.type}</p>
+	</div>
+</a>
---
+++ src/routes/+page.server.js
@@ -0,0 +1,31 @@
+import { API_KEY } from '$env/static/private';
+import axios from 'axios';
+/** @type {import('./$types').PageLoad} */
+
+const options = {
+	method: 'GET',
+	params: {
+		page: '1',
+		size: '30'
+	},
+	headers: {
+		'X-RapidAPI-Key': API_KEY,
+		'X-RapidAPI-Host': 'anime-db.p.rapidapi.com'
+	}
+};
+
+export async function load() {
+	try {
+		const response = await axios.get(
+			'https://anime-db.p.rapidapi.com/anime?page=1&size=30',
+			options
+		);
+		let animes = response.data.data;
+		let initialData = response.data.data;
+		let loading = false;
+		console.log(animes);
+		return { animes, initialData, loading };
+	} catch (error) {
+		console.log(error.message);
+	}
+}
