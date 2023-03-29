9eacb9858c34b2d97d4b0271795a6189ba793e4f
Ludovic <ludovicli973@gmail.com>
2023-03-29 10:56:50 +0200

    Created Header component

---
+++ src/lib/header.svelte
@@ -0,0 +1,17 @@
+<div class="w-full h-16 flex items-center space-x-4">
+	<a href="/">
+		<img src="/zenika_logo.png" alt="Logo Zenika" class="h-full aspect-auto w-[90px]" />
+	</a>
+	<ul>
+		<li><a href="/">Valeurs</a></li>
+	</ul>
+	<ul>
+		<li><a href="/">Offres</a></li>
+	</ul>
+	<ul>
+		<li><a href="/">Green IT</a></li>
+	</ul>
+	<ul>
+		<li><a href="/">Sécurité</a></li>
+	</ul>
+</div>
