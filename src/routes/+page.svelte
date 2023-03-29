39a0ca345885c7de547073eb376656522edbf5cc
IssamLynx <hm_bakha@esi.dz>
2023-03-29 16:49:56 +0200

    Finished home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Card from '$lib/Card.svelte';
+
+	export let data;
+	let isLoading = true;
+	let animes = data.animes;
+	//Use initialData for search functionnality
+	let initialData = data.animes;
+	isLoading = data.loading;
+</script>
+
+<div>
+	{#if isLoading === true}
+		<p>En cours de chargement...</p>
+	{:else if animes.length > 0}
+		<div class="my-6 flex flex-wrap justify-around py-1 flex-row">
+			{#each animes as anime, index}
+				<div class="my-10" key={index}>
+					<Card {anime} />
+				</div>
+			{/each}
+		</div>
+	{:else}
+		<p>Not Found</p>
+	{/if}
+</div>
