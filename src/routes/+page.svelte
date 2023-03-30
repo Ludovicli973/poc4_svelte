b9f7df2021e7ab4a401d950183c29c0b7a459696
edouardglx <edouard.gloux@zenika.com>
2023-03-30 11:01:57 +0200

    Added logic to search Anime

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,26 @@
 <script>
 	import Card from '$lib/Card.svelte';
-
+	import SearchBar from '../lib/SearchBar.svelte';
 	export let data;
 	let isLoading = true;
 	let animes = data.animes;
-	//Use initialData for search functionnality
 	let initialData = data.animes;
 	isLoading = data.loading;
+
+	const HandleSearch = (e) => {
+		const filterDataForSearch = initialData.filter((obj) => {
+			return obj.title.toLowerCase().includes(e.detail.toLowerCase());
+		});
+		animes = filterDataForSearch;
+	};
+	const HandelClear = () => {
+		animes = data.animes;
+	};
 </script>
 
 <div>
-	{#if isLoading}
+	<SearchBar on:clear-select={HandelClear} on:search-select={HandleSearch} />
+	{#if isLoading === true}
 		<p>En cours de chargement...</p>
 	{:else if animes.length > 0}
 		<div class="my-6 flex flex-wrap justify-around py-1 flex-row">
