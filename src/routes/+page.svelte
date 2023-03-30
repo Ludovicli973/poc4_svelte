fda1e29d4b583f75abbd1784195d4db8a90c40e9
IssamLynx <hm_bakha@esi.dz>
2023-03-30 10:01:27 +0200

    Refactored code and removed vitest

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <div>
-	{#if isLoading === true}
+	{#if isLoading}
 		<p>En cours de chargement...</p>
 	{:else if animes.length > 0}
 		<div class="my-6 flex flex-wrap justify-around py-1 flex-row">
