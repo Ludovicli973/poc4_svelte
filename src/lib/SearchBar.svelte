0c545a3fbc1fe2bb6f25a7ca41100b1cd989abf2
edouardglx <edouard.gloux@zenika.com>
2023-03-30 11:01:57 +0200

    Added logic to search Anime

--- src/lib/SearchBar.svelte
+++ src/lib/SearchBar.svelte
@@ -1,26 +1,42 @@
 <script>
-	//recuperer data depuis le parent
-	//creer une valeur reactive pour l'input
-	//faire remonter l'event au click sur le button search, prend en parametre la valeur de l'input
-	//pour le button clear, si la valeur de l'input est plus grand que 0, on l'affiche sinon non.
-	//au clic sur click, on supprime la valeur de l'input et on reaffiche le tableau de 30 animes
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	//VARIABLE INPUT
+	let searchInput = '';
+	//SUBMIT
+	const handleSubmit = () => {
+		dispatch('search-select', searchInput);
+	};
+	//CLEAR
+	const handleClear = () => {
+		searchInput = '';
+		dispatch('clear-select');
+	};
 </script>
 
 <div class="flex items-center mt-2 ml-4">
-	<form>
+	<form on:submit|preventDefault={handleSubmit}>
 		<input
 			type="text"
 			name="input"
+			bind:value={searchInput}
 			class="w-[200px] h-[30px] border border-black rounded"
 			placeholder="Search"
 		/>
 		<button
 			class="ml-[10px] w-[100px] h-[30px] border border-black rounded bg-black text-white cursor-pointer"
+			type="submit"
 		>
 			Search
 		</button>
-		<button class="ml-[10px] w-[100px] h-[30px] border rounded bg-gray-200 border-gray-200">
-			Clear
-		</button>
+		{#if searchInput.trim().length > 0}
+			<button
+				class="ml-[10px] w-[100px] h-[30px] border rounded bg-gray-200 border-gray-200"
+				on:click={handleClear}
+			>
+				Clear
+			</button>
+		{/if}
 	</form>
 </div>
