5068a292cafe761ce015d6441694d66f48b79623
edouardglx <edouard.gloux@zenika.com>
2023-03-30 12:24:34 +0200

    resolved conflict

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
 
 <div>
 	<SearchBar on:clear-select={HandelClear} on:search-select={HandleSearch} />
-	{#if isLoading === true}
+	{#if isLoading}
 		<p>En cours de chargement...</p>
 	{:else if animes.length > 0}
 		<div class="my-6 flex flex-wrap justify-around py-1 flex-row">
