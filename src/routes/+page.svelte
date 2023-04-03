611f5aba4918b10992280fa7e89b5036445662a6
Ludovic <ludovicli973@gmail.com>
2023-04-03 12:13:28 +0200

    Corrected the name of the methods

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,19 +11,19 @@
 	initialData = data.animes;
 	isLoading = data.loading;
 
-	const HandleSearch = (e) => {
+	const handleSearch = (e) => {
 		const filterDataForSearch = initialData.filter((obj) => {
 			return obj.title.toLowerCase().includes(e.detail.toLowerCase());
 		});
 		animes = filterDataForSearch;
 	};
-	const HandelClear = () => {
+	const handleClear = () => {
 		animes = data.animes;
 	};
 </script>
 
 <div>
-	<SearchBar on:clear-select={HandelClear} on:search-select={HandleSearch} />
+	<SearchBar on:clear-select={handleClear} on:search-select={handleSearch} />
 	{#if isLoading}
 		<p>En cours de chargement...</p>
 	{:else if animes.length > 0}
