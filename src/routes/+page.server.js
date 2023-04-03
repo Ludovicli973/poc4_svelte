da2f4b5324383a2cb30db6b09b7a30e7ec26df36
Ludovic <ludovicli973@gmail.com>
2023-04-03 12:05:25 +0200

    Removed axios from the +page.server.js file and replaced it by fetch

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,5 +1,4 @@
 import { API_KEY } from '$env/static/private';
-import axios from 'axios';
 
 const options = {
 	method: 'GET',
@@ -14,15 +13,13 @@ const options = {
 };
 
 export async function load() {
-	let animes;
-	let loading;
 	try {
-		const response = await axios.get(
+		const response = await fetch(
 			'https://anime-db.p.rapidapi.com/anime?page=1&size=30',
 			options
-		);
-		animes = response.data.data;
-		loading = false;
+		).then((res) => res.json());
+		let animes = response.data;
+		let loading = false;
 		return { animes, loading };
 	} catch (error) {
 		console.error(error.message);
