5e3fd0808b60f518437ab111992a6c9854964854
Ludovic <ludovicli973@gmail.com>
2023-04-05 15:26:13 +0200

    Deployment file updated and corrected href link
    Deployment file updated and corrected href link
    
    Modified config file and added deployment.yaml for github actions
    
    Deployment file updated
    
    modified the deployment process
    
    Modified the deployment file
    
    updated the deployment file to add create_empty_branch
    
    removed the deprecated option
    
    changed the name of the branch
    
    Modified the trigger

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,8 +1,9 @@
 <script>
 	export let anime;
+	import { base } from '$app/paths';
 </script>
 
-<a href={`/details/${anime._id}`}>
+<a href={`${base}/details/${anime._id}`}>
 	<div class="w-[200px] h-[270px] my-1">
 		<img src={anime.image} alt="poster" class="w-full h-full object-cover" />
 		<p>{anime.title}</p>
--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,17 +1,21 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <div class="w-full h-16 flex items-center space-x-4">
-	<a href="/">
-		<img src="/zenika_logo.png" alt="Logo Zenika" class="h-full aspect-auto w-[90px]" />
+	<a href={`${base}/`}>
+		<img src={`${base}/zenika_logo.png`} alt="Logo Zenika" class="h-full aspect-auto w-[90px]" />
 	</a>
 	<ul>
-		<li><a href="/">Valeurs</a></li>
+		<li><a href={`${base}/`}>Valeurs</a></li>
 	</ul>
 	<ul>
-		<li><a href="/">Offres</a></li>
+		<li><a href={`${base}/`}>Offres</a></li>
 	</ul>
 	<ul>
-		<li><a href="/">Green IT</a></li>
+		<li><a href={`${base}/`}>Green IT</a></li>
 	</ul>
 	<ul>
-		<li><a href="/">Sécurité</a></li>
+		<li><a href={`${base}/`}>Sécurité</a></li>
 	</ul>
 </div>
