6d1a954dcc1d71f49adaefb0215051ec856a6cdf
Ludovic <ludovicli973@gmail.com>
2023-04-07 09:36:10 +0200

    New config file

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,4 @@
-// import adapter from '@sveltejs/adapter-auto';
-// import { vitePreprocess } from '@sveltejs/kit/vite';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-// 		adapter: adapter()
-// 	},
-// 	preprocess: vitePreprocess()
-// };
-// export default config;
-
 import adapter from '@sveltejs/adapter-static';
-const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -22,13 +6,8 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
+			fallback: null
 		})
-		// paths: {
-		// 	base: dev ? '' : '/poc4_svelte'
-		// }
 	},
 	vite: {
 		envPrefix: ''
